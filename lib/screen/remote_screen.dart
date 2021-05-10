ae55dc8c8a3b0b1cb3334991f8aaf2897dce62b9
Tenzin Tamdin <38410384+tentamdin@users.noreply.github.com>
2021-05-11 01:40:56 +0530

    remote Ui complete

--- lib/screen/remote_screen.dart
+++ lib/screen/remote_screen.dart
@@ -10,113 +10,181 @@ class _RemoteScreenState extends State<RemoteScreen> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: [
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-              Container(
-                decoration: BoxDecoration(
-                  shape: BoxShape.rectangle,
-                  borderRadius: BorderRadius.circular(15),
-                  border: Border.all(
-                      color: Colors.grey, width: 2, style: BorderStyle.solid),
-                ),
-                height: 150,
-                width: 55,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    IconButton(
-                      icon: Icon(
-                        Icons.add,
-                      ),
-                      onPressed: () {},
+              Material(
+                elevation: 2,
+                borderRadius: BorderRadius.circular(50),
+                child: Container(
+                  width: 50,
+                  decoration: BoxDecoration(
+                    color: Colors.white30,
+                    shape: BoxShape.circle,
+                  ),
+                  child: IconButton(
+                    onPressed: () {},
+                    icon: Icon(
+                      Icons.keyboard,
                     ),
-                    Text("VOL"),
-                    IconButton(
-                      icon: Icon(
-                        Icons.remove,
-                      ),
-                      onPressed: () {},
-                    )
-                  ],
+                  ),
                 ),
               ),
-              Container(
-                width: 150,
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  border: Border.all(
-                      color: Colors.grey, width: 2, style: BorderStyle.solid),
-                ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    IconButton(
-                      icon: Icon(
-                        Icons.keyboard_arrow_up,
-                      ),
-                      onPressed: () {},
+              Material(
+                elevation: 2,
+                borderRadius: BorderRadius.circular(50),
+                child: Container(
+                    height: 50,
+                    width: 50,
+                    decoration: BoxDecoration(
+                      color: Colors.white30,
+                      shape: BoxShape.circle,
                     ),
-                    Row(
-                      children: [
-                        IconButton(
-                          icon: Icon(
-                            Icons.keyboard_arrow_left,
-                          ),
-                          onPressed: () {},
-                        ),
-                        IconButton(
-                          icon: Icon(
-                            Icons.circle,
-                            color: Colors.grey,
-                          ),
-                          onPressed: () {},
-                        ),
-                        IconButton(
-                          icon: Icon(
-                            Icons.keyboard_arrow_right,
-                          ),
-                          onPressed: () {},
-                        ),
-                      ],
-                    ),
-                    IconButton(
-                      icon: Icon(
-                        Icons.keyboard_arrow_down,
-                      ),
+                    child: IconButton(
                       onPressed: () {},
-                    )
-                  ],
+                      icon: Icon(
+                        Icons.power_settings_new,
+                      ),
+                    )),
+              ),
+              Material(
+                elevation: 2,
+                borderRadius: BorderRadius.circular(50),
+                child: Container(
+                    height: 50,
+                    width: 50,
+                    decoration: BoxDecoration(
+                      color: Colors.white30,
+                      shape: BoxShape.circle,
+                    ),
+                    child: IconButton(
+                      onPressed: () {},
+                      icon: Icon(
+                        Icons.keyboard_voice_rounded,
+                      ),
+                    )),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Material(
+                elevation: 2,
+                borderRadius: BorderRadius.circular(15),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    shape: BoxShape.rectangle,
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  height: 160,
+                  width: 56,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      IconButton(
+                        icon: Icon(
+                          Icons.add,
+                        ),
+                        onPressed: () {},
+                      ),
+                      Text("VOL"),
+                      IconButton(
+                        icon: Icon(
+                          Icons.remove,
+                        ),
+                        onPressed: () {},
+                      )
+                    ],
+                  ),
                 ),
               ),
-              Container(
-                height: 150,
-                width: 55,
-                decoration: BoxDecoration(
-                  shape: BoxShape.rectangle,
-                  borderRadius: BorderRadius.circular(15),
-                  border: Border.all(
-                      color: Colors.grey, width: 2, style: BorderStyle.solid),
+              Material(
+                elevation: 2,
+                borderRadius: BorderRadius.circular(50),
+                child: Container(
+                  height: 160,
+                  width: 160,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    shape: BoxShape.rectangle,
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      IconButton(
+                        icon: Icon(
+                          Icons.keyboard_arrow_up,
+                        ),
+                        onPressed: () {},
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          IconButton(
+                            icon: Icon(
+                              Icons.keyboard_arrow_left,
+                            ),
+                            onPressed: () {},
+                          ),
+                          IconButton(
+                            icon: Icon(
+                              Icons.circle,
+                              color: Colors.grey,
+                            ),
+                            onPressed: () {},
+                          ),
+                          IconButton(
+                            icon: Icon(
+                              Icons.keyboard_arrow_right,
+                            ),
+                            onPressed: () {},
+                          ),
+                        ],
+                      ),
+                      IconButton(
+                        icon: Icon(
+                          Icons.keyboard_arrow_down,
+                        ),
+                        onPressed: () {},
+                      )
+                    ],
+                  ),
                 ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    IconButton(
-                      icon: Icon(
-                        Icons.keyboard_arrow_up,
+              ),
+              Material(
+                elevation: 2,
+                borderRadius: BorderRadius.circular(20),
+                child: Container(
+                  height: 160,
+                  width: 56,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    shape: BoxShape.rectangle,
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      IconButton(
+                        icon: Icon(
+                          Icons.keyboard_arrow_up,
+                        ),
+                        onPressed: () {},
                       ),
-                      onPressed: () {},
-                    ),
-                    Text("CH"),
-                    IconButton(
-                      icon: Icon(
-                        Icons.keyboard_arrow_down,
-                      ),
-                      onPressed: () {},
-                    )
-                  ],
+                      Text("CH"),
+                      IconButton(
+                        icon: Icon(
+                          Icons.keyboard_arrow_down,
+                        ),
+                        onPressed: () {},
+                      )
+                    ],
+                  ),
                 ),
               )
             ],
@@ -131,14 +199,9 @@ class _RemoteScreenState extends State<RemoteScreen> {
                   height: 50,
                   width: 80,
                   decoration: BoxDecoration(
-                    color: Color(0xFFEDEDED),
+                    color: Colors.white,
                     shape: BoxShape.rectangle,
                     borderRadius: BorderRadius.circular(15),
-                    border: Border.all(
-                      color: Colors.grey,
-                      width: 0.1,
-                      style: BorderStyle.solid,
-                    ),
                   ),
                   child: IconButton(
                     icon: Icon(
@@ -155,14 +218,9 @@ class _RemoteScreenState extends State<RemoteScreen> {
                   height: 50,
                   width: 130,
                   decoration: BoxDecoration(
-                    color: Color(0xFFEDEDED),
+                    color: Colors.white,
                     shape: BoxShape.rectangle,
                     borderRadius: BorderRadius.circular(15),
-                    border: Border.all(
-                      color: Colors.grey,
-                      width: 0.1,
-                      style: BorderStyle.solid,
-                    ),
                   ),
                   child: IconButton(
                     icon: Icon(
@@ -179,14 +237,9 @@ class _RemoteScreenState extends State<RemoteScreen> {
                   height: 50,
                   width: 80,
                   decoration: BoxDecoration(
-                    color: Color(0xFFEDEDED),
+                    color: Colors.white,
                     shape: BoxShape.rectangle,
                     borderRadius: BorderRadius.circular(15),
-                    border: Border.all(
-                      color: Colors.grey,
-                      width: 0.1,
-                      style: BorderStyle.solid,
-                    ),
                   ),
                   child: IconButton(
                     icon: Icon(
@@ -198,7 +251,79 @@ class _RemoteScreenState extends State<RemoteScreen> {
               )
             ],
           ),
-          Row(),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Material(
+                elevation: 2,
+                borderRadius: BorderRadius.circular(20),
+                child: Container(
+                  height: 70,
+                  width: 70,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    shape: BoxShape.rectangle,
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Image.asset('assets/netflix_logo.png'),
+                  ),
+                ),
+              ),
+              Material(
+                elevation: 2,
+                borderRadius: BorderRadius.circular(15),
+                child: Container(
+                  height: 70,
+                  width: 70,
+                  decoration: BoxDecoration(
+                    color: Colors.white30,
+                    shape: BoxShape.rectangle,
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Image.asset('assets/prime_logo.jpeg'),
+                  ),
+                ),
+              ),
+              Material(
+                elevation: 2,
+                borderRadius: BorderRadius.circular(15),
+                child: Container(
+                  height: 70,
+                  width: 70,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    shape: BoxShape.rectangle,
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Image.asset('assets/disney-plus-logo.png'),
+                  ),
+                ),
+              ),
+              Material(
+                elevation: 2,
+                borderRadius: BorderRadius.circular(15),
+                child: Container(
+                  height: 70,
+                  width: 70,
+                  decoration: BoxDecoration(
+                    color: Colors.white30,
+                    shape: BoxShape.rectangle,
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Image.asset('assets/spotify_logo.png'),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ],
       ),
     );
